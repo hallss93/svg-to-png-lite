883993514751c1007b52747b9db3db23123a85eb
Hallison Melo <hallss93@hotmail.com>
2019-06-27 08:48:38 -0300

    Adiccionado Opções

--- README.md
+++ README.md
@@ -10,4 +10,28 @@ _Start:_
 2. Importe a biblioteca:
 ````javascript
 import svgpng from 'svg-to-png-lite';
+````
+Exemplo 1:
+````javascript
+// Passe o ID do SVG
 svgpng("svg-id").then(console.log).catch(console.error)
+````
+
+Exemplo 2:
+````javascript
+// Determinando Width e Height - Passando ID e Opções
+svgpng("svg-id", {width:300, height:300}).then(console.log).catch(console.error)
+
+## OPÇÕES
+
+### WIDTH `Number`.  Default `largura do SVG`
+
+Determina a largura da imagem.
+
+### HEIGHT `Number`. Default `altura do SVG`
+
+Determina a altura da imagem.
+
+### EXT `String`. Default `PNG`
+
+Determina a extensão da imagem\: PNG ou JPEG
--- index.js
+++ index.js
@@ -1,26 +1,43 @@
 'use strict';
-module.exports = function (id) {
+module.exports = function (id, opt) {
 	return new Promise((res, rej) => {
-		let doc = document.querySelector("#" + id);
-		var svgString = new XMLSerializer().serializeToString(doc);
-		var image = new Image();
-		let xml = window.btoa(unescape(encodeURIComponent(svgString)));
-		image.src = "data:image/svg+xml;base64," + xml;
-		let url = "";
-
-		image.onload = function () {
-			var canvas = document.createElement("canvas");
-			canvas.width = image.width;
-			canvas.height = image.height;
-			var context = canvas.getContext("2d");
-			context.drawImage(image, 0, 0);
-			url = canvas.toDataURL("image/png");
-			res(url);
-		};
-		image.onerror = function (e) {
+		const {
+			width,
+			height,
+			ext = "png"
+		} = opt
+		if (ext.toUpperCase() != 'PNG' && ext.toUpperCase() != 'JPEG') {
 			rej({
-				error: e
+				error: "Extensão inválida"
 			})
-		}
+			return false;
+		} else
+			try {
+				let doc = document.querySelector("#" + id);
+				var svgString = new XMLSerializer().serializeToString(doc);
+				var image = new Image();
+				let xml = window.btoa(unescape(encodeURIComponent(svgString)));
+				image.src = "data:image/svg+xml;base64," + xml;
+				let url = "";
+
+				image.onload = function () {
+					var canvas = document.createElement("canvas");
+					canvas.width = width ? width : image.width;
+					canvas.height = height ? height : image.height;
+					var context = canvas.getContext("2d");
+					context.drawImage(image, 0, 0);
+					url = canvas.toDataURL("image/" + ext);
+					res(url);
+				};
+				image.onerror = function (e) {
+					rej({
+						error: e
+					})
+				}
+			} catch (err) {
+				rej({
+					error: err
+				})
+			}
 	})
 }
